4529ebbf8a52f0237e656bee13c3ebe536feb7d1
kingminor <70523148+kingminor@users.noreply.github.com>
2024-10-11 10:46:53 -0600

    Added Health Script / Created Systems Folder
    Created Health Script.

---
+++ Assets/Systems/Health.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] public float startingHealth; //Starting Health For the Character
+
+    private float currentHealth; //Current Health for the Character
+
+    void Start () {
+        currentHealth = startingHealth; //Initializes Current Health
+    }
+
+    public void TakeDamage (float damageTaken) {
+        currentHealth = currentHealth - damageTaken; //Subtracts damage equal to float input.
+
+        if(currentHealth <= 0)
+        {
+            KillCharacter();
+        }
+    }
+
+    private void KillCharacter() {
+        //Placeholder Function
+        Debug.Log(gameObject.name + "Is now Dead");
+        return;
+    }
+}
