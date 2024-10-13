e6e8add2e0586c8cbc8efd435b167ad0c3cf5b10
Sunpine522 <koalapokemonfun@gmail.com>
2024-10-12 19:27:26 -0600

    Traps now hide/unhide

--- Assets/Systems/TrapActivation.cs
+++ Assets/Systems/TrapActivation.cs
@@ -8,12 +8,25 @@ public class TrapActivation : MonoBehaviour
     [SerializeField] string damageType;
     [SerializeField] List<GameObject> validObjects;
     public DotScript dotScript;
+    bool hidden;
+    [SerializeField] Sprite cover;
+    [SerializeField] Sprite unCovered;
 
+    public void Start()
+    {
+        hidden = true;
+        gameObject.GetComponent<SpriteRenderer>().sprite = cover;
+    }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (validObjects.Contains(collision.gameObject))
         {
+            if (hidden)
+            {
+                hidden = false;
+                gameObject.GetComponent<SpriteRenderer>().sprite = unCovered;
+            }
             dotScript.InflictDoT(stacks, damageType, collision.gameObject);
         }
     }
